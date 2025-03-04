41e3a990706b5cdbe22052b92a153a280875b43a
Neville Brem <contact@nevillebrem.com>
2025-03-04 12:35:58 +0100

    move to online turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+import 'dotenv/config';
+
+if (!process.env.DATABASE_URL || !process.env.DATABASE_TOKEN)
+	throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/db/schema/*.sql.ts',
 	out: './drizzle',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: process.env.DATABASE_URL,
+		authToken: process.env.DATABASE_TOKEN
 	},
 
 	verbose: true,
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,10 +1,10 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema/index';
 import { createClient } from '@libsql/client';
-import { DATABASE_URL } from '$env/static/private';
+import { DATABASE_TOKEN, DATABASE_URL } from '$env/static/private';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: DATABASE_URL });
+const client = createClient({ url: DATABASE_URL, authToken: DATABASE_TOKEN });
 
 export const db = drizzle(client, { schema });
